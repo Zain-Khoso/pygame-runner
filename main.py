6297920b4063954a183f984c90836e9ba7d40d44
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-01 15:30:55 +0500

    new: Converting .png to pygame 'objects'

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load("graphics/Sky.png")
-groung_surface = pygame.image.load("graphics/ground.png")
+sky_surface = pygame.image.load("graphics/Sky.png").convert()
+groung_surface = pygame.image.load("graphics/ground.png").convert()
 text_surface = test_font.render("Hello World", False, "Black")
 
-snail_surface = pygame.image.load("graphics/snail/snail1.png")
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_x_pos = 800
 
 while True:
