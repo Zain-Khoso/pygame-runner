3aa986922f3180997b127f1f7b4b5d31e4111fe9
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-01 15:18:46 +0500

    new: Font/Text loading

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ pygame.init()
 pygame.display.set_caption("Runner")
 screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+
 sky_surface = pygame.image.load("graphics/Sky.png")
 groung_surface = pygame.image.load("graphics/ground.png")
+text_surface = test_font.render("Hello World", False, "Black")
 
 while True:
     for event in pygame.event.get():
@@ -16,6 +19,7 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(groung_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
 
     pygame.display.update()
     clock.tick(60)
