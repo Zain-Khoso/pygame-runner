fbccd41bf387bd2c3cbdd9678f47093db17efcd4
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-01 14:33:26 +0500

    update: framerate

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 pygame.init()
 pygame.display.set_caption("Runner")
 screen = pygame.display.set_mode((800, 400))
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
@@ -12,3 +13,4 @@ while True:
             exit()
 
     pygame.display.update()
+    clock.tick(60)
