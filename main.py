e615c1062ed296b82caee0fb3221cb27496caea8
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-01 15:05:24 +0500

    new: Custom surface

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ pygame.init()
 pygame.display.set_caption("Runner")
 screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
+test_surface = pygame.Surface((100, 200))
+test_surface.fill("Blue")
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(test_surface, (0, 0))
 
     pygame.display.update()
     clock.tick(60)
