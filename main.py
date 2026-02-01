f3518913df51a2c3ff57d420521e86124e55d52b
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-01 14:24:10 +0500

    fix: pygame secure exit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sys import exit
 import pygame
 
 pygame.init()
@@ -7,5 +8,6 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
 
     pygame.display.update()
