9bea3f6f71c25dae841f892a89429d5f61ec5f2d
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-01 15:52:26 +0500

    fix: snail movement

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ groung_surface = pygame.image.load("graphics/ground.png").convert()
 text_surface = test_font.render("Hello World", False, "Black")
 
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-snail_rect = snail_surface.get_rect(midbottom=(800, 300))
+snail_rect = snail_surface.get_rect(bottomleft=(800, 300))
 
 player_suface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
 player_rect = player_suface.get_rect(midbottom=(50, 300))
@@ -27,8 +27,8 @@ while True:
     screen.blit(groung_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
 
-    snail_rect.left -= 4
-    if snail_rect.left < -100:
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
         snail_rect.left = 800
 
     screen.blit(snail_surface, snail_rect)
