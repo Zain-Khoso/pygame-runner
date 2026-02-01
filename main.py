3fe3d56060e95e3d26ecda725dc9ff5a25752d13
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-01 15:53:32 +0500

    fix: variables surface -> surf

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load("graphics/Sky.png").convert()
-groung_surface = pygame.image.load("graphics/ground.png").convert()
-text_surface = test_font.render("Hello World", False, "Black")
+sky_surf = pygame.image.load("graphics/Sky.png").convert()
+groung_surf = pygame.image.load("graphics/ground.png").convert()
+text_surf = test_font.render("Hello World", False, "Black")
 
-snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-snail_rect = snail_surface.get_rect(bottomleft=(800, 300))
+snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_rect = snail_surf.get_rect(bottomleft=(800, 300))
 
-player_suface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
-player_rect = player_suface.get_rect(midbottom=(50, 300))
+player_surf = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(50, 300))
 
 while True:
     for event in pygame.event.get():
@@ -23,16 +23,16 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(groung_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(groung_surf, (0, 300))
+    screen.blit(text_surf, (300, 50))
 
     snail_rect.x -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
 
-    screen.blit(snail_surface, snail_rect)
-    screen.blit(player_suface, player_rect)
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
 
     pygame.display.update()
     clock.tick(60)
