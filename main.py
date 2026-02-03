828dd039099f8924d40778693d1bf7490ff9fcea
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-03 13:56:49 +0500

    fix: Obsticle object moved to a separate file.

--- main.py
+++ main.py
@@ -1,48 +1,9 @@
 from sys import exit
-from random import randint, choice
+from random import choice
 import pygame
 
-class Obsticle(pygame.sprite.Sprite):
-    def __init__(self, type):
-        super().__init__()
-
-        if type == "Snail":
-            self.frames = [
-                pygame.image.load("graphics/snail/snail1.png").convert_alpha(),
-                pygame.image.load("graphics/snail/snail2.png").convert_alpha(),
-            ]
-
-        if type == "Fly":
-            self.frames = [
-                pygame.image.load("graphics/Fly/Fly1.png").convert_alpha(),
-                pygame.image.load("graphics/Fly/Fly2.png").convert_alpha(),
-            ]
-
-        y_pos = 210 if type == "Fly" else 300
-
-        self.frame = 0
-        self.image = self.frames[self.frame]
-        self.rect = self.image.get_rect(midbottom=(randint(900, 1100), y_pos))
-
-    def animate(self):
-        self.frame += 0.1
-
-        if self.frame >= len(self.frames):
-            self.frame = 0
-
-        self.image = self.frames[int(self.frame)]
-
-    def destroy(self):
-        if self.rect.x < -100:
-            self.kill()
-
-    def update(self):
-        self.rect.x -= 5
-
-        self.animate()
-        self.destroy()
-
 from player import Player
+from obsticle import Obsticle
 
 def display_score():
     current_time = pygame.time.get_ticks() - start_time
---
+++ obsticle.py
@@ -0,0 +1,46 @@
+# Imports.
+from random import randint
+import pygame
+
+
+# Obsticle object definition.
+class Obsticle(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+
+        if type == "Snail":
+            self.frames = [
+                pygame.image.load("graphics/snail/snail1.png").convert_alpha(),
+                pygame.image.load("graphics/snail/snail2.png").convert_alpha(),
+            ]
+
+        if type == "Fly":
+            self.frames = [
+                pygame.image.load("graphics/Fly/Fly1.png").convert_alpha(),
+                pygame.image.load("graphics/Fly/Fly2.png").convert_alpha(),
+            ]
+
+        y_pos = 210 if type == "Fly" else 300
+
+        self.frame = 0
+        self.image = self.frames[self.frame]
+        self.rect = self.image.get_rect(midbottom=(randint(900, 1100), y_pos))
+
+    def animate(self):
+        self.frame += 0.1
+
+        if self.frame >= len(self.frames):
+            self.frame = 0
+
+        self.image = self.frames[int(self.frame)]
+
+    def destroy(self):
+        if self.rect.x < -100:
+            self.kill()
+
+    def update(self):
+        self.rect.x -= 5
+
+        self.animate()
+        self.destroy()
+
